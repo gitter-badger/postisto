7cc3dff6da246f1433931af8a20942c57b421cdb
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2020-01-04 15:40:49 +0100

    test bad port
    Signed-off-by: Arnold Bechtoldt <arnold.bechtoldt@inovex.de>

--- pkg/conn/conn_test.go
+++ pkg/conn/conn_test.go
@@ -15,15 +15,21 @@ func TestConnect(t *testing.T) {
 	redisClient, err := integration.NewRedisClient()
 	require.Nil(err)
 	accs := map[string]*config.Account{
-		"main": integration.NewAccount(10143, true, false, true),
-		"main_imaps": integration.NewAccount(10993, false, true, true),
+		"starttls": integration.NewAccount(10143, true, false, true),
+		"starttls_wrongport": integration.NewAccount(42, true, false, true),
+		"imaps": integration.NewAccount(10993, false, true, true),
+		"imaps_wrongport": integration.NewAccount(42, false, true, true),
 	}
 
-	require.Nil(integration.NewIMAPUser(accs["main"], redisClient))
-	require.Nil(integration.NewIMAPUser(accs["main_imaps"], redisClient))
+	require.Nil(integration.NewIMAPUser(accs["starttls"], redisClient))
+	require.Nil(integration.NewIMAPUser(accs["starttls_wrongport"], redisClient), )
+	require.Nil(integration.NewIMAPUser(accs["imaps"], redisClient))
+	require.Nil(integration.NewIMAPUser(accs["imaps_wrongport"], redisClient))
 
-	require.Nil(Connect(accs["main"]))
-	require.Nil(Connect(accs["main_imaps"]))
+	require.Nil(Connect(accs["starttls"]))
+	assert.Error(Connect(accs["starttls_wrongport"]))
+	require.Nil(Connect(accs["imaps"]))
+	assert.Error(Connect(accs["imaps_wrongport"]))
 
 	defer func() {
 		for _, err := range DisconnectAll(accs) { //TODO verify whether accs actually contians all accs

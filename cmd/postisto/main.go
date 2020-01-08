57583c8b87429873221b8ff794ea59e277027532
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2020-01-08 17:11:19 +0100

    fix gitignore
    Signed-off-by: Arnold Bechtoldt <arnold.bechtoldt@inovex.de>

---
+++ cmd/postisto/main.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+	//// NewConfigFromFile user config
+	//var err error
+	//cfg := config.New()
+	//if cfg, err = cfg.NewConfigFromFile("/Users/ab/Documents/dev/GOPATH/src/github.com/arnisoph/postisto/test/data/configs/valid/"); err != nil {
+	//	log.Panicf("failed to load config: %v", err)
+	//}
+	//
+	//// Connect to IMAP servers
+	//accs := map[string]*config.Account{}
+	//for accName, acc := range cfg.Accounts {
+	//	if !acc.Connection.Enabled {
+	//		continue
+	//	}
+	//	if err := conn.Connect(acc); err != nil {
+	//		log.Fatalf("failed to connect (%v): %v", accName, err)
+	//
+	//	} else {
+	//		accs[accName] = acc
+	//	}
+	//}
+	//
+	//defer func() {
+	//	for _, acc := range accs {
+	//		if err := conn.Disconnect(acc); err != nil {
+	//			log.Fatalf("failed to discoonect account %v", err)
+	//		}
+	//	}
+	//}()
+	//
+	//for _, acc := range accs {
+	//	log.Println(acc.Connection.Client.State(), imap.AuthenticatedState)
+	//}
+	//
+	//log.Println("Done!")
+}
